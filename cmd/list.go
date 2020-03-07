453d1bf9d0b9fa8b5f6bc3719d5729546ebad847
Theo Salvo <buzzsurfr@gmail.com>
2020-03-07 11:03:27 -0500

    Move init() to bottom of file

--- cmd/list.go
+++ cmd/list.go
@@ -75,20 +75,6 @@ func listFunc(cmd *cobra.Command, args []string) {
 	// fmt.Printf("%s\n", result)
 }
 
-func init() {
-	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
 func getSessionContext(sess *session.Session) (string, error) {
 	var userName string
 
@@ -177,4 +163,18 @@ func obfuscateString(s string, n int) string {
 		}
 	}
 	return ret
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
